4f3cab62ca601443a0c25cc0a5c9bf540ab9f23b
ViranchiPaliwal <viranchi.paliwal@gmail.com>
2018-07-04 02:50:58 -0700

    created ui and styling Assignment

--- components/Assignment.js
+++ components/Assignment.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
-import {View} from 'react-native'
-import {Text, ListItem} from 'react-native-elements'
+import {View, ScrollView, TextInput} from 'react-native'
+import style from '../styles/styles';
+import {Text, Button, FormLabel, FormInput, FormValidationMessage} from 'react-native-elements'
 
 class Assignment extends Component {
     static navigationOptions = {
@@ -11,21 +12,75 @@ class Assignment extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            courses: []
+            topicId: '',
+            title: '',
+            description: '',
+            points: 0
         }
+    }
+
+    componentDidMount() {
+        const {navigation} = this.props;
+        const topicId = navigation.getParam("topicId")
+        this.setState({topicId:topicId})
+    }
+
+    update(updatedState){
+        this.setState(updatedState);
+    }
+
+    saveAssignment(){
+        let assignment = {
+            title: this.state.title,
+            description: this.state.description,
+            points: this.state.points,
+            widgetType: 'Assignment'
+        }
+
     }
 
     render() {
         return(
-            <View style={{padding: 15}}>
-                {this.state.courses.map
-                ((course, index) => (
-                    <ListItem
-                        title={course.title}
-                        key={index}
-                        onPress={() => this.props.navigation.navigate("ModuleList", {courseId: course.id})}/>
-                ))}
-            </View>
+            <ScrollView>
+                <FormLabel>Title</FormLabel>
+                <FormInput onChangeText={ text => this.update({title: text})}/>
+                <FormValidationMessage> Title is required </FormValidationMessage>
+
+                <FormLabel>Description</FormLabel>
+                <FormInput onChangeText={ text => this.update({description: text})}/>
+                <FormValidationMessage> Description is required </FormValidationMessage>
+
+                <FormLabel>Points</FormLabel>
+                <FormInput onChangeText={ points => this.update({points: points}) }/>
+                <FormValidationMessage> Points is required </FormValidationMessage>
+
+                <Button style={style.buttonStyle}
+                        backgroundColor="green"
+                        color="white"
+                        title="Save"
+                        onPress={() => {this.saveAssignment()}}/>
+                <Button style={style.buttonStyle}
+                       backgroundColor="red"
+                       color="white"
+                       title="Cancel"
+                       onPress={() => { this.props.navigation
+                                        .navigate("WidgetList", {lessonId: this.state.lessonId})
+                                      }}/>
+                <Text h2>Preview</Text>
+                <View style={style.viewStyle}>
+                <Text h3>{this.state.title}</Text>
+                <Text h4>{this.state.points}</Text>
+                </View>
+                <Text >{this.state.description}</Text>
+
+                <Text h4>Essay Answer</Text>
+                <TextInput style={style.essayStyle}/>
+
+                <Text h3>Upload a file</Text>
+
+                <Text h3>Submit a Link</Text>
+
+            </ScrollView>
         )
     }
 }
---
+++ styles/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native'
+export default StyleSheet.create({
+    viewStyle: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        flex: 1,
+        flexWrap: 'wrap'
+    },
+    essayStyle: {
+        height: 100,
+        borderColor: 'gray',
+        borderWidth: 0.5,
+        backgroundColor:'white'
+    },
+    buttonStyle: {
+        padding:5,
+        borderRadius: 5
+    }
+})
